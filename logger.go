8eb5b6f0f78b52d581137d1df304ee6cf94c894c
Aditya Gupta <adityagupta2087@gmail.com>
2026-03-01 20:36:31 +0530

    prettier logs

--- logger.go
+++ logger.go
@@ -3,13 +3,13 @@ package main
 import "log"
 
 func logInfo(message string) {
-	log.Printf("info: %s\n", message) 
+	log.Printf("[INFO]: %s\n", message) 
 }
 
 func logErr(message string) {
-	log.Printf("error: %s\n", message)
+	log.Printf("[ERROR]: %s\n", message)
 }
 
 func logFatal(message string) {
-	log.Printf("fatal: %s\n", message) 
+	log.Printf("[FATAL]: %s\n", message) 
 }
